e1fe9b4a9a6dc8d608af54487e7809b59e5ba837
Vladimir Chernitsyn <flunt1k@yandex-team.ru>
2023-03-10 18:29:28 +0100

    feat(AdaptiveTabs story): add adaptive tab story

---
+++ src/components/__stories__/AdaptiveTabs.stories.tsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import {Meta, Story} from '@storybook/react';
+import {AdaptiveTabs, AdaptiveTabsProps} from '../AdaptiveTabs/AdaptiveTabs';
+export default {
+    title: 'Components/AdaptiveTabs',
+    component: AdaptiveTabs,
+} as Meta;
+const Template: Story<AdaptiveTabs> = (args) => {
+    const [activeTab, setActiveTab] = useState('active');
+    const items: AdaptiveTabsProps['items'] = React.useMemo(
+        () => [
+            {
+                id: 'first',
+                title: 'First Tab',
+            },
+            {
+                id: 'active',
+                title: 'Active Tab',
+            },
+            {
+                id: 'disabled',
+                title: 'Disabled With Long Text Tab',
+                disabled: true,
+            },
+            {
+                id: 'fourth',
+                title: 'Fourth Long Text To Show Tab',
+            },
+            {
+                id: 'fifth',
+                title: 'One More Long Text Tab To Show',
+            },
+        ],
+        [],
+    );
+
+    return (
+        <div style={{resize: 'horizontal', overflow: 'auto'}}>
+            <AdaptiveTabs
+                {...args}
+                items={items}
+                onSelectTab={setActiveTab}
+                activeTab={activeTab}
+            />
+        </div>
+    );
+};
+export const Default = Template.bind({});
