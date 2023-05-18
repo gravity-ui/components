5ec0565aec2dd5b18f6962b775330f708ba8a3f5
marginy <98329176+Marginy605@users.noreply.github.com>
2023-05-18 12:55:07 +0300

    fix(PlaceholderComponent): fix export (#48)

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export * from './AdaptiveTabs';
 export * from './FormRow';
 export * from './InfiniteScroll';
 export * from './ItemSelector';
+export * from './PlaceholderContainer';
 
 export {Lang, configure} from './utils/configure';
