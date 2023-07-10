9b350237adab7e92f7c240fcc72d61a8946633e8
Andrey Morozov <amje@yandex-team.ru>
2023-07-10 18:59:16 +0300

    feat!: update uikit

--- src/components/Notifications/__stories__/Notifications.stories.tsx
+++ src/components/Notifications/__stories__/Notifications.stories.tsx
@@ -12,7 +12,7 @@ const Template: ComponentStory<typeof Notifications> = (args) => (
     <div
         style={{
             borderRadius: '8px',
-            border: '1px solid var(--yc-color-line-generic)',
+            border: '1px solid var(--g-color-line-generic)',
             margin: '4px',
             width: '350px',
             height: '470px',
