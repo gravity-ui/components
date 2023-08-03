e780af7de5f8a7d64967453e39ff1948a386837c
el-makarova <el-makarova@yandex-team.ru>
2023-08-03 17:45:20 +0300

    feat: story with a problem

--- src/components/AdaptiveTabs/__stories__/AdaptiveTabs.stories.tsx
+++ src/components/AdaptiveTabs/__stories__/AdaptiveTabs.stories.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-import {Meta, Story} from '@storybook/react';
+import {Meta, Story, StoryFn} from '@storybook/react';
 
 import {AdaptiveTabs, AdaptiveTabsProps} from '../AdaptiveTabs';
 export default {
@@ -55,3 +55,53 @@ const Template: Story<{size: 'm' | 'l' | 'xl'} & AdaptiveTabs<{}>> = (args) => {
     );
 };
 export const Default = Template.bind({});
+
+const WrapTemplate: StoryFn<AdaptiveTabsProps<{}>> = (args) => {
+    const [activeTab, setActiveTab] = useState('active');
+    const items: AdaptiveTabsProps<{}>['items'] = React.useMemo(
+        () => [
+            {
+                id: 'first',
+                title: 'First Tab',
+            },
+            {
+                id: 'active',
+                title: 'Active Tab',
+            },
+            {
+                id: 'disabled',
+                title: 'Disabled With Long Text Tab',
+                disabled: true,
+            },
+            {
+                id: 'fourth',
+                title: 'Fourth Long Text To Show Tab',
+            },
+            {
+                id: 'fifth',
+                title: 'One More Long Text Tab To Show',
+            },
+        ],
+        [],
+    );
+
+    return (
+        <div style={{resize: 'horizontal', overflow: 'auto'}}>
+            <AdaptiveTabs
+                {...args}
+                items={items}
+                onSelectTab={setActiveTab}
+                activeTab={activeTab}
+                wrapTo={(_, node) => {
+                    return (
+                        <div style={{display: 'flex', alignItems: 'center'}}>
+                            <span>extra</span>
+                            {node}
+                        </div>
+                    );
+                }}
+            />
+        </div>
+    );
+};
+export const Wrap = WrapTemplate.bind({});
