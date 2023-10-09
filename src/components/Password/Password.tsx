810becd08324a05509a7778ba7ba153ae8ed7928
NasgulNexus <mostovoyegor@yandex-team.ru>
2023-10-09 13:11:19 +0200

    feat(Password): made a new input password

---
+++ src/components/Password/Password.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+import {Copy, CopyCheck, Eye, EyeSlash} from '@gravity-ui/icons';
+import {
+    Button,
+    CopyToClipboard,
+    CopyToClipboardStatus,
+    Icon,
+    TextInput,
+    TextInputProps,
+} from '@gravity-ui/uikit';
+import _ from 'lodash';
+
+import {block} from '../utils/cn';
+
+import './Password.scss';
+
+const b = block('password');
+
+export type PasswordProps = Required<Pick<TextInputProps, 'onUpdate' | 'value'>> &
+    Omit<TextInputProps, 'rightContent' | 'type'> & {
+        showCopyButton?: boolean;
+    };
+
+export const Password: React.FC<PasswordProps> = (props) => {
+    const {autoComplete, value, showCopyButton} = props;
+
+    const [hideValue, setHideValue] = React.useState(true);
+
+    const additionalRightContent = React.useMemo(() => {
+        const onClick = () => {
+            setHideValue((hideValue) => !hideValue);
+        };
+
+        return (
+            <div className={b()}>
+                {value && showCopyButton ? (
+                    <CopyToClipboard text={String(value)} timeout={500}>
+                        {(state) => (
+                            <Button view="flat-secondary" className={b('button')} size="s">
+                                <Icon
+                                    size={14}
+                                    data={
+                                        state === CopyToClipboardStatus.Pending ? Copy : CopyCheck
+                                    }
+                                />
+                            </Button>
+                        )}
+                    </CopyToClipboard>
+                ) : null}
+                <Button view="flat-secondary" onClick={onClick} className={b('button')} size="s">
+                    <Icon data={hideValue ? Eye : EyeSlash} size={14} />
+                </Button>
+            </div>
+        );
+    }, [hideValue, value, showCopyButton]);
+
+    return (
+        <TextInput
+            {...props}
+            type={hideValue ? 'password' : 'text'}
+            rightContent={additionalRightContent}
+            autoComplete={autoComplete ? autoComplete : 'new-password'}
+        />
+    );
+};
