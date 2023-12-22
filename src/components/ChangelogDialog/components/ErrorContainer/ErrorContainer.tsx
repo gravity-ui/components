ba3f0db04a91f699bfe03be344453b991d0efb6d
Anton Vikulov <rndnm@yandex-team.ru>
2023-12-22 16:33:17 +0500

    fix(ChangelogDialog): error view

--- src/components/ChangelogDialog/components/ErrorContainer/ErrorContainer.tsx
+++ src/components/ChangelogDialog/components/ErrorContainer/ErrorContainer.tsx
@@ -1,42 +1,32 @@
-import React, {FC, useMemo} from 'react';
+import React from 'react';
 
-import {TriangleExclamation} from '@gravity-ui/icons';
-import {Icon} from '@gravity-ui/uikit';
+import {Alert} from '@gravity-ui/uikit';
 
-import {PlaceholderContainer} from '../../../PlaceholderContainer';
-import {block} from '../../../utils/cn';
 import i18n from '../../i18n';
-
-import './ErrorContainer.scss';
-
-const b = block('changelog-dialog-error-container');
 
 interface ErrorContainerProps {
     onRetryClick?: () => void;
     error?: boolean | {title?: string; description?: string};
 }
 
-export const ErrorContainer: FC<ErrorContainerProps> = ({onRetryClick, error}) => {
-    const {title, description} = useMemo(() => {
+export function ErrorContainer({onRetryClick, error}: ErrorContainerProps) {
+    const {title, description} = React.useMemo(() => {
         return error && typeof error === 'object' ? error : {};
     }, [error]);
 
     return (
-        <PlaceholderContainer
-            size={'s'}
+        <Alert
             title={title || i18n('label_error-title')}
-            description={description}
-            image={
-                <div className={b('error-image-container')}>
-                    <Icon data={TriangleExclamation} className={b('error-image')} size={32} />
-                </div>
-            }
-            action={
-                onRetryClick && {
-                    text: i18n('button_retry'),
-                    handler: onRetryClick,
-                }
+            message={description}
+            theme={'danger'}
+            actions={
+                onRetryClick && [
+                    {
+                        text: i18n('button_retry'),
+                        handler: onRetryClick,
+                    },
+                ]
             }
         />
     );
-};
+}
