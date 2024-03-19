b15771f7258a762ddbea5ace6bc9343045e6474f
Anton Vikulov <rndnm@yandex-team.ru>
2024-03-19 17:29:26 +0500

    fix(lint): ignore useEffect in decorator

--- .storybook/decorators/withLang.tsx
+++ .storybook/decorators/withLang.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
+import {settings} from '@gravity-ui/date-utils';
 import {configure} from '@gravity-ui/uikit';
 import type {Decorator} from '@storybook/react';
-import {settings} from '@gravity-ui/date-utils';
 
 export const withLang: Decorator = (Story, context) => {
     const lang = context.globals.lang;
     configure({lang});
 
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     React.useEffect(() => {
         (async () => {
             await settings.loadLocale(lang);
