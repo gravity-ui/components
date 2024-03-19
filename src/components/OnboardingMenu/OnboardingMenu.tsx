e296ed47792533ad88881f82234979eebd536a7c
nikita-jpg <56046344+nikita-jpg@users.noreply.github.com>
2024-03-19 18:01:32 +0300

    feat: add OnboardingMenu component (#121)

---
+++ src/components/OnboardingMenu/OnboardingMenu.tsx
@@ -0,0 +1,141 @@
+import React from 'react';
+
+import {Button, Icon, IconData, Progress} from '@gravity-ui/uikit';
+
+import {block} from '../utils/cn';
+
+import {OnboardingMenuItem} from './components/OnboardingMenuItem';
+import {OnboardingMenuItemImage} from './components/OnboardingMenuItemImage';
+import {OnboardingMenuItemText} from './components/OnboardingMenuItemText';
+import i18n from './i18n';
+
+import './OnboardingMenu.scss';
+
+const cnOnboardingMenu = block('onboarding-menu');
+
+export type OnboardingMenuProps = {
+    title: React.ReactNode;
+    progress: number;
+    icon?: IconData;
+
+    defaultExpanded: boolean;
+    children?: React.ReactNode;
+
+    collapseButtonText?: string;
+    completeButtonText?: string;
+    className?: string;
+
+    onExpand?: (expanded: boolean) => void;
+    onCompleteClick: (event: React.MouseEvent) => void;
+    onCollapseClick?: (event: React.MouseEvent) => void;
+};
+
+const OnboardingMenuParent = ({
+    title,
+    progress,
+    icon,
+    defaultExpanded,
+    onExpand,
+    onCollapseClick,
+    onCompleteClick,
+    collapseButtonText,
+    completeButtonText,
+    className,
+    children,
+}: OnboardingMenuProps) => {
+    const [expand, setExpand] = React.useState(defaultExpanded);
+
+    const onExpandCallback = () => {
+        const newExpand = !expand;
+
+        setExpand(newExpand);
+        if (onExpand) {
+            onExpand(newExpand);
+        }
+    };
+
+    const onCollapseClickCallback = (event: React.MouseEvent) => {
+        const newExpand = !expand;
+
+        setExpand(newExpand);
+        if (onCollapseClick) {
+            onCollapseClick(event);
+        }
+        if (onExpand) {
+            onExpand(newExpand);
+        }
+    };
+
+    return (
+        <div className={cnOnboardingMenu(null, className)}>
+            <div className={cnOnboardingMenu('content', {expand: expand})}>
+                <div className={cnOnboardingMenu('header', {expand: expand})}>
+                    <button
+                        className={cnOnboardingMenu('title', {expand: expand})}
+                        onClick={onExpandCallback}
+                        tabIndex={0}
+                    >
+                        {icon ? (
+                            <Icon
+                                data={icon}
+                                width={20}
+                                height={20}
+                                className={cnOnboardingMenu('title-icon')}
+                            />
+                        ) : null}
+
+                        {/* <span>
+                            {title}
+                        </span> */}
+                        {title}
+                    </button>
+                    <Progress
+                        size={expand ? 's' : 'xs'}
+                        stack={[
+                            {
+                                value: progress,
+                                color: 'var(--g-color-base-positive-heavy)',
+                            },
+                            {
+                                value: 100 - progress,
+                                color: 'var(--g-color-base-background)',
+                            },
+                        ]}
+                    />
+                </div>
+
+                {expand && (
+                    <React.Fragment>
+                        <div className={cnOnboardingMenu('lining')}>{children}</div>
+                        <div className={cnOnboardingMenu('buttons')}>
+                            <Button
+                                view="normal-contrast"
+                                size="m"
+                                onClick={onCollapseClickCallback}
+                                width="max"
+                            >
+                                {collapseButtonText ?? i18n('collapse')}
+                            </Button>
+                            <Button
+                                view="outlined-contrast"
+                                onClick={onCompleteClick}
+                                size="m"
+                                width="max"
+                            >
+                                {completeButtonText ?? i18n('complete')}
+                            </Button>
+                        </div>
+                    </React.Fragment>
+                )}
+            </div>
+        </div>
+    );
+};
+
+OnboardingMenuParent.displayName = 'OnboardingMenu';
+
+export const OnboardingMenu = Object.assign(OnboardingMenuParent, {
+    Item: OnboardingMenuItem,
+    ItemText: OnboardingMenuItemText,
+    ItemImage: OnboardingMenuItemImage,
+});
---
+++ src/components/OnboardingMenu/components/OnboardingMenuItem.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+import {CircleCheckFill, Clock} from '@gravity-ui/icons';
+import {ArrowToggle, Disclosure, Icon, Skeleton} from '@gravity-ui/uikit';
+
+import {block} from '../../utils/cn';
+
+import './OnboardingMenuItem.scss';
+
+export type OnboardingMenuItemProps = {
+    title: string;
+    status?: 'completed' | 'pending';
+    children?: React.ReactNode;
+    buttons?: React.ReactNode;
+    hasDivider?: boolean;
+    loading?: boolean;
+};
+
+const ICON_SIZE = 20;
+
+const cnOnboardingMenuItem = block('onboarding-menu-item');
+
+export const OnboardingMenuItem = ({
+    title,
+    status,
+    children,
+    buttons,
+    hasDivider = true,
+    loading = false,
+}: OnboardingMenuItemProps) => {
+    const icon = React.useMemo(() => {
+        if (status === 'pending') {
+            return (
+                <Icon
+                    size={ICON_SIZE}
+                    data={Clock}
+                    className={cnOnboardingMenuItem('status-icon_pending')}
+                />
+            );
+        }
+
+        if (status === 'completed') {
+            return (
+                <Icon
+                    size={ICON_SIZE}
+                    data={CircleCheckFill}
+                    className={cnOnboardingMenuItem('status-icon')}
+                />
+            );
+        }
+
+        return (
+            <Icon
+                size={ICON_SIZE}
+                data={CircleCheckFill}
+                className={cnOnboardingMenuItem('status-icon_disabled')}
+            />
+        );
+    }, []);
+
+    const summary = (
+        <div className={cnOnboardingMenuItem('collapse-header')}>
+            <div className={cnOnboardingMenuItem('icon')}>{icon}</div>
+            <span className={cnOnboardingMenuItem('title')}>{title}</span>
+        </div>
+    );
+
+    const header = children ? (
+        <Disclosure size="l" className={cnOnboardingMenuItem('title-header')}>
+            <Disclosure.Summary>
+                {(props) => (
+                    <button {...props} className={cnOnboardingMenuItem('title-summary')}>
+                        {summary}
+                        <ArrowToggle direction={props.expanded ? 'top' : 'bottom'} />
+                    </button>
+                )}
+            </Disclosure.Summary>
+            <div className={cnOnboardingMenuItem('node')}>{children}</div>
+            {buttons && <div className={cnOnboardingMenuItem('buttons')}>{buttons}</div>}
+        </Disclosure>
+    ) : (
+        summary
+    );
+
+    if (loading) {
+        return (
+            <React.Fragment>
+                <div className={cnOnboardingMenuItem('skeleton')}>
+                    <Skeleton
+                        className={cnOnboardingMenuItem('skeleton-avatar')}
+                        style={{width: `${ICON_SIZE}px`, height: `${ICON_SIZE}px`}}
+                    />
+                    <Skeleton className={cnOnboardingMenuItem('skeleton-title')} />
+                </div>
+                {hasDivider && <div className={cnOnboardingMenuItem('divider')}></div>}
+            </React.Fragment>
+        );
+    }
+
+    return (
+        <div className={cnOnboardingMenuItem()}>
+            <div className={cnOnboardingMenuItem('collapse')}>{header}</div>
+            {hasDivider && <div className={cnOnboardingMenuItem('divider')}></div>}
+        </div>
+    );
+};
