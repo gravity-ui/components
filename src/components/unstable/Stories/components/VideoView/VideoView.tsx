d94415383aad4725928181e669f2aba345160d60
Roman Barlos <rbarlos@yandex-team.ru>
2024-12-09 17:35:40 +0300

    feat(Stories): add unstable versions of Stories and StoriesGroup components

---
+++ src/components/unstable/Stories/components/VideoView/VideoView.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {block} from '../../../../utils/cn';
+import type {MediaRendererProps} from '../MediaRenderer/MediaRenderer';
+
+import './VideoView.scss';
+
+const b = block('stories-video-view');
+
+export interface VideoViewProps extends MediaRendererProps {}
+
+export function VideoView({media, style}: VideoViewProps) {
+    if (media.type === 'video') {
+        return (
+            <div className={b({style})}>
+                <video
+                    className={b('video')}
+                    src={media.url}
+                    controls={false}
+                    playsInline
+                    muted
+                    autoPlay
+                    loop
+                    poster={media.posterUrl}
+                    // eslint-disable-next-line react/no-unknown-property
+                    webkit-playsinline="true"
+                />
+            </div>
+        );
+    }
+
+    return null;
+}
---
+++ src/components/unstable/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable camelcase */
+export {
+    type StoriesItemMedia as unstable_StoriesItemMedia,
+    type StoriesItem as unstable_StoriesItem,
+    type StoriesProps as unstable_StoriesProps,
+    Stories as unstable_Stories,
+    useSyncWithLS as unstable_useSyncWithLS,
+} from './Stories';
