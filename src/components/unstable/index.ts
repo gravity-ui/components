70fdf1c07ba666ffbef8c996b90924ecf03fe5f5
Roman Barlos <rbarlos@yandex-team.ru>
2024-12-16 18:59:34 +0300

    fix(unstable): add missed exports

--- src/components/unstable/index.ts
+++ src/components/unstable/index.ts
@@ -5,4 +5,13 @@ export {
     type StoriesProps as unstable_StoriesProps,
     Stories as unstable_Stories,
     useSyncWithLS as unstable_useSyncWithLS,
+    StoriesMediaBlockStyle,
+    StoriesTextBlockStyle,
+    StoriesItemTextColorStyles,
 } from './Stories';
+
+export {
+    type StoriesGroupItem,
+    type StoriesGroupProps,
+    StoriesGroup as unstable_StoriesGroup,
+} from './StoriesGroup';
