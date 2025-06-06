b935a6d174a4fa34d0bf4542425a75143ecb2ca2
Pelageia Fadeeva <pelageyafad@yandex-team.ru>
2025-06-06 11:57:11 +0300

    feat: add StoreBadgeNext (fix lint)

--- src/components/StoreBadgeNext/StoreBadgeNext.tsx
+++ src/components/StoreBadgeNext/StoreBadgeNext.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 
 import {Icon, Link, LinkProps} from '@gravity-ui/uikit';
 
+import {AppGalleryColor} from '../../icons/stores/AppGalleryColor';
+import {AppGalleryMono} from '../../icons/stores/AppGalleryMono';
 import {AppStoreColor} from '../../icons/stores/AppStoreColor';
 import {AppStoreMono} from '../../icons/stores/AppStoreMono';
 import {GooglePlayColor} from '../../icons/stores/GooglePlayColor';
 import {GooglePlayMono} from '../../icons/stores/GooglePlayMono';
-import {AppGalleryColor} from '../../icons/stores/AppGalleryColor';
-import {AppGalleryMono} from '../../icons/stores/AppGalleryMono';
 import {RuStoreColor} from '../../icons/stores/RuStoreColor';
 import {RuStoreMono} from '../../icons/stores/RuStoreMono';
 import {cn} from '../utils/cn';
@@ -19,24 +19,24 @@ const appstorePlatform = 'appstore';
 const huaweiPlatform = 'huaweystore';
 const rustorePlatform = 'rustore';
 
-const color = 'color';
-const monochrome = 'monochrome';
+export type StoreBadgePlatform =
+    | typeof playmarketPlatform
+    | typeof appstorePlatform
+    | typeof huaweiPlatform
+    | typeof rustorePlatform;
 
-export type StoreBadgePlatform = typeof playmarketPlatform | typeof appstorePlatform | typeof huaweiPlatform | typeof rustorePlatform;
-
-export type appearanceColor = typeof color | typeof monochrome;
+export type StoreBadgeAppearance = 'color' | 'monochrome';
 
 export interface StoreBadgeNextProps extends Omit<LinkProps, 'view' | 'onClick'> {
     /** store platform name */
     store: StoreBadgePlatform;
-    appearance?: appearanceColor;
+    appearance?: StoreBadgeAppearance;
     onClick?: React.MouseEventHandler<HTMLElement>;
 }
 
-
 const badgeData: Record<
     StoreBadgePlatform,
-    Record<appearanceColor, React.FunctionComponent<React.SVGProps<SVGSVGElement>>>
+    Record<StoreBadgeAppearance, React.FunctionComponent<React.SVGProps<SVGSVGElement>>>
 > = {
     [appstorePlatform]: {
         color: AppStoreColor,
@@ -61,7 +61,7 @@ export const StoreBadgeNext = ({
     className,
     onClick,
     href,
-    appearance = color,
+    appearance = 'color',
     ...restLinkProps
 }: StoreBadgeNextProps) => {
     const iconData = badgeData?.[store][appearance];
