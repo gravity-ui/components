138d0301c9575259ea0c8eadd35688dd1919fd36
Taya Leutina <leutinatasya@yandex-team.ru>
2025-06-10 17:29:02 +0300

    chore: debug

--- src/components/Gallery/components/views/ImageView/ImageView.tsx
+++ src/components/Gallery/components/views/ImageView/ImageView.tsx
@@ -46,6 +46,8 @@ export const ImageView = ({className, src, alt = '', mobile}: ImageViewProps) =>
 
     const mobileProps = mobile ? {style: mobileAnimationStyle, onDoubleClick} : {};
 
+    alert(`mobile: ${mobile}, mobileProps: ${JSON.stringify(mobileProps)}`);
+
     return (
         <React.Fragment>
             {status === 'loading' && <Spin className={cnImageView('spin')} size="xl" />}
