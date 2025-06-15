cb8aa9b28577dd478787035c6a1c7c70cfb3fc8c
Taya Leutina <leutinatasya@yandex-team.ru>
2025-06-15 18:28:29 +0300

    fix: move DefaultNameBlock in components dir

--- src/components/Gallery/utils/getGalleryItemDocument.tsx
+++ src/components/Gallery/utils/getGalleryItemDocument.tsx
@@ -2,7 +2,7 @@ import {FilePreview} from '@gravity-ui/uikit';
 import type {FilePreviewProps} from '@gravity-ui/uikit';
 
 import type {GalleryItemProps} from '../GalleryItem';
-import {DefaultNameBlock} from '../components/views/DefaultNameBlock/DefaultNameBlock';
+import {DefaultNameBlock} from '../components/DefaultNameBlock/DefaultNameBlock';
 import {DocumentView} from '../components/views/DocumentView/DocumentView';
 import type {DocumentViewProps} from '../components/views/DocumentView/DocumentView';
 
