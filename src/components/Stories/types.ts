043e6261a1931e8df27b1d59915e97f152e6cd67
Elena Martynova <marginy@yandex-team.ru>
2025-06-26 13:20:44 +0300

    fix(Stories): add react node type to description (#304)

--- src/components/Stories/types.ts
+++ src/components/Stories/types.ts
@@ -15,7 +15,7 @@ export type StoriesItemMedia = {url: string} & (
 export interface StoriesItem {
     title?: string;
     /** @deprecated use `content` property instead */
-    description?: string;
+    description?: string | React.ReactNode;
     content?: React.ReactNode;
     /** Url for link "more" */
     url?: string;
