bffef8102564d847513493aa2ef69938728c5629
Leonid21B <lbardash@yandex-team.ru>
2025-08-07 08:20:40 +1200

    feat(Gallery): add callback to close action

--- src/hooks/useGallery/useGallery.tsx
+++ src/hooks/useGallery/useGallery.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 
 import {GalleryContext} from './GalleryContext';
 
-export function useGallery() {
-    return React.useContext(GalleryContext);
+export function useGallery(onCloseCallback?: () => void) {
+    const {openGallery, closeGallery: contextClose} = React.useContext(GalleryContext);
+
+    const closeGallery = React.useCallback(() => {
+        contextClose();
+        onCloseCallback?.();
+    }, [contextClose]);
+
+    return {openGallery, closeGallery};
 }
