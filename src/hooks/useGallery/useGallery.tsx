1602ae333eb2a23f1fb1e7dd5ad637ce3e8bc410
Leonid21B <lbardash@yandex-team.ru>
2025-08-09 09:22:27 +1200

    feat(Gallery): return isopen from hook

--- src/hooks/useGallery/useGallery.tsx
+++ src/hooks/useGallery/useGallery.tsx
@@ -2,13 +2,6 @@ import * as React from 'react';
 
 import {GalleryContext} from './GalleryContext';
 
-export function useGallery(onCloseCallback?: () => void) {
-    const {openGallery, closeGallery: contextClose} = React.useContext(GalleryContext);
-
-    const closeGallery = React.useCallback(() => {
-        contextClose();
-        onCloseCallback?.();
-    }, [contextClose]);
-
-    return {openGallery, closeGallery};
+export function useGallery() {
+    return React.useContext(GalleryContext);
 }
